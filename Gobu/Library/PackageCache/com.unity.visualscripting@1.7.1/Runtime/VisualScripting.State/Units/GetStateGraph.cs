a7ad24a3c73c7656157e70e80ee82b92ffd141ec
Ain10 <chaosknight2223@gmail.com>
2021-07-07 02:37:22 +0800

    Delete
    Delete all

--- Gobu/Library/PackageCache/com.unity.visualscripting@1.7.1/Runtime/VisualScripting.State/Units/GetStateGraph.cs
+++
@@ -1,8 +0,0 @@
-namespace Unity.VisualScripting
-{
-    /// <summary>
-    /// Get a StateGraphAsset from a GameObject
-    /// </summary>
-    [TypeIcon(typeof(StateGraphAsset))]
-    public class GetStateGraph : GetGraph<StateGraph, StateGraphAsset, StateMachine> { }
-}
--- Gobu/Library/PackageCache/com.unity.visualscripting@1.7.1/Runtime/VisualScripting.State/Units/GetStateGraphs.cs
+++
@@ -1,8 +0,0 @@
-namespace Unity.VisualScripting
-{
-    /// <summary>
-    /// Get a list of all the StateGraphs from a GameObject
-    /// </summary>
-    [TypeIcon(typeof(StateGraphAsset))]
-    public class GetStateGraphs : GetGraphs<StateGraph, StateGraphAsset, StateMachine> { }
-}
